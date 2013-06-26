37cec3da25c16dcdbea6c6b9514bd66d060a86ca
Takayuki Muranushi <muranushi@gmail.com>
2013-06-26 18:58:03 +0900

    hayashi model in the paper.

--- paper7/src/Paper/SectionModel.hs
+++ paper7/src/Paper/SectionModel.hs
@@ -4,10 +4,11 @@ module Paper.SectionModel where
 import           Control.Monad.Author
 import qualified Text.LaTeX as LTX
 
+import           Model.Disk.Hayashi (hayashiModelDoc)
 
 sectionModel :: Monad m => AuthorT m ()
 sectionModel = do
   LTX.section "Model"
-  "distribution was shown."
-  2 * 3.14e96
+  hayashiModelDoc
+  
 

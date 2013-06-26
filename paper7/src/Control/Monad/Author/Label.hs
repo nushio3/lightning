5290597f4ea17e27b701ceeca65380065fc142ee
Takayuki Muranushi <muranushi@gmail.com>
2013-06-26 14:05:59 +0900

    label provider implemented.

--- paper7/src/Control/Monad/Author/Label.hs
+++ paper7/src/Control/Monad/Author/Label.hs
@@ -6,8 +6,12 @@ data Label
   = FromType !TypeRep 
   | FromStr !String
   | Ap !Label !Label
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
 
+instance Show Label where
+  show (FromType r) = show r
+  show (FromStr s) = s
+  show (Ap a b) = show a ++ show b
 
 (</) :: Show a => Label -> a -> Label
 l </ x = Ap l (FromStr $ show x)
--- paper7/src/Control/Monad/Author/Ref.hs
+++ paper7/src/Control/Monad/Author/Ref.hs
@@ -2,19 +2,43 @@
 module Control.Monad.Author.Ref where
 
 
-import           Control.Applicative
-import           Control.Lens.Getter (use)
+import           Control.Lens (use, (%=))
 import qualified Control.Monad.Author.State as AS
 import           Control.Monad.Author.Label (Label)
 import           Control.Monad.State.Strict (MonadState(..))
 import           Text.LaTeX (rendertex)
 import qualified Text.LaTeX as LTX
 import           Text.LaTeX.Base.Class (LaTeXC(..))
+import           Data.Text (Text, pack)
 import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 
-ref :: (MonadState AS.Root m, LaTeXC (m a), Functor m) => Label -> m a
+getRef :: (MonadState AS.Root m) => Label -> m Text
+getRef lab = do
+  map0 <- use AS.labelMap
+  case Map.lookup lab map0 of
+    Just str -> return str
+    Nothing  -> do
+      let cands :: [String]
+          cands = cand0 : [s ++ [c]| s <- cands, c <- ['a'..'z']]
+          cand0 = show lab
+
+          takens :: Set.Set Text
+          takens = Set.fromList $ Map.elems map0
+          
+          isTaken x = Set.member (pack x) takens
+
+          freeStr :: Text
+          freeStr = pack $ head $ filter (not . isTaken) cands
+      AS.labelMap %= (Map.insert lab freeStr)
+      return freeStr
+
+ref :: (MonadState AS.Root m, LaTeXC (m a)) => Label -> m a
 ref lab = do
-  ret <- Map.lookup lab <$> use AS.labelMap
-  case ret of
-    Just str -> LTX.ref $ rendertex str
-    Nothing -> undefined
+  ret <- getRef lab
+  LTX.ref $ rendertex ret
+
+label :: (MonadState AS.Root m, LaTeXC (m a)) => Label -> m a
+label lab = do
+  ret <- getRef lab
+  LTX.label $ rendertex ret

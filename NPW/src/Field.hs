8a3c076a057c6f98eea82546dc8cbcd23b68c87e
Takayuki Muranushi <muranushi@gmail.com>
2013-08-19 14:55:56 +0900

    symmetric

--- NPW/src/Field.hs
+++ NPW/src/Field.hs
@@ -6,16 +6,19 @@ import           Data.Word
 
 
 type Field r e = R.Array r R.DIM2 e
+type System = (Field R.U Int,  Field R.U Double)
+
 type RGB = (Word8,Word8,Word8)
 
 type Edge = (R.DIM2, R.DIM2)
 
     
     
-plot :: Field R.U Int -> Field R.U Double -> Field R.U RGB                    
-plot bc pot =
+plot :: System -> Field R.U RGB                    
+plot (bc, pot) =
   runIdentity $ R.computeUnboxedP $ R.zipWith go bc pot
   where
+    go (-2) _ = (255,255,255)
     go (-1) _ = (0,0,64)
     go 1    _ = (64,0,0)
     go _    v = (rb(v*128+128),rb(128-v*128),255)

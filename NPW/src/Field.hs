74a87675ba156081665b0dd0b3d3079166987265
Takayuki Muranushi <muranushi@gmail.com>
2013-08-19 13:25:19 +0900

    test edges

--- NPW/src/Field.hs
+++ NPW/src/Field.hs
@@ -1,9 +1,24 @@
 module Field where
 
+import           Control.Monad.Identity
 import qualified Data.Array.Repa as R
-import qualified Data.Map as M
 import           Data.Word
 
 
 type Field r e = R.Array r R.DIM2 e
 type RGB = (Word8,Word8,Word8)
+
+type Edge = (R.DIM2, R.DIM2)
+
+    
+    
+plot :: Field R.U Int -> Field R.U Double -> Field R.U RGB                    
+plot bc pot =
+  runIdentity $ R.computeUnboxedP $ R.zipWith go bc pot
+  where
+    go (-1) _ = (0,0,64)
+    go 1    _ = (64,0,0)
+    go _    v = (rb(v*128+128),rb(128-v*128),255)
+    rb = round . max 0 . min 255
+    
+    

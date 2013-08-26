aa3ed7d20c46cf77e97745cd7bf7d9f9b78b9c31
Takayuki Muranushi <muranushi@gmail.com>
2013-08-26 19:46:16 +0900

    introduce citation resolver state.

--- paper7/src/Control/Monad/Author/State.hs
+++ paper7/src/Control/Monad/Author/State.hs
@@ -7,13 +7,15 @@ import           Control.Lens.TH (makeLenses)
 import           Data.Default
 import           Data.Text (Text)
 import qualified Data.Map.Strict as Map
+import qualified Text.CSL.Input.Identifier.Internal as Citation
 
-data Root 
-  = Root 
+data Root
+  = Root
   { _labelMap :: Map.Map Label Text
-  } deriving (Eq, Show)
+  , _citationDB :: Citation.DB
+  }
 
 $(makeLenses ''Root)
 
 instance Default Root where
-  def = Root def
+  def = Root def def

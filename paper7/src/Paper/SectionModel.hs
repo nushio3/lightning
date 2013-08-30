46662de98561ac94ae3606c855e2463145538b64
Takayuki Muranushi <muranushi@gmail.com>
2013-08-30 13:35:23 +0900

    citation resolver included.

--- paper7/src/Paper/SectionModel.hs
+++ paper7/src/Paper/SectionModel.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Paper.SectionModel where
 
 import           Control.Monad.Author
+import           Control.Monad.Author.Cite(cite)
 import           Control.Monad.RWS
 import qualified Text.LaTeX as LTX
 import qualified Text.LaTeX.Utils as LTX
-
+import           Text.LaTeX.Base.Class (LaTeXC(..))
 import           Model.Disk.Hayashi (hayashiModelDoc)
 
-sectionModel :: Monad m => AuthorT m ()
+sectionModel :: (MonadIO m) => AuthorT m ()
 sectionModel = do
   tell $ LTX.section "Model"
   tell $ LTX.citet "hayashi_structure_1981"
+  () <- cite "bibcode:1981PThPS..70...35H"
   hayashiModelDoc

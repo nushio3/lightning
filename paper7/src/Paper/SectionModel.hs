2741bdef6d6a2bac206c26f68a0c86fa994f2d16
nushio <nushio@nushiohshio>
2013-10-10 14:56:15 +0000

    organize the three models of lightnings.

--- paper7/src/Paper/SectionModel.hs
+++ paper7/src/Paper/SectionModel.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Paper.SectionModel where
 
 import           Control.Monad.RWS
+import           HereDocument (doc)
 import           Model.Disk.Hayashi (hayashiModelDoc)
   
 import           Text.Authoring
@@ -14,15 +17,34 @@ sectionModel = do
   command1 "section" $ raw "Model"
   esc $ "We study the following three models of lightning: "
   
-  esc "Conventional breakdown model, used e.g. in "
-  citet ["doi:10.1006/icar.1999.6245"]
-  raw ".\n\n"
+  environment "itemize" $ do
+
+    command0 "item"
+    
+    esc $ [doc| 
+           Conventional breakdown model, widely used in meteorological context,
+           and also adopted into astrophysical context e.g. by |]
+    citet ["doi:10.1006/icar.1999.6245"]
+    raw ".\n\n"
+
+    command0 "item"
+
+    esc $ [doc| 
+           Druyversteyn-Penning breakdown model, based on electrical discharge 
+           model proposed by |]
+    citet ["doi:10.1103/RevModPhys.12.87"]
+    esc " and introduced as a protoplanetary disk lightning by"
+    citet ["doi:10.1086/432796"]
+    raw ".\n\n"
+
+
+    command0 "item"
+
+    esc "Runaway breakdown model, proposed by "
+    citet ["doi:10.1016/0375-9601(92)90348-P","doi:10.1070/PU2001v044n11ABEH000939"]
+    raw ".\n\n"
   
-  esc "Runaway breakdown model, proposed by "
-  citet ["doi:10.1016/0375-9601(92)90348-P","doi:10.1070/PU2001v044n11ABEH000939"]
-  raw ".\n\n"
-  
-  esc "Cross section data are taken from"
+  esc "Cross section data are taken from "
   citet ["isbn:3-540-64296-X","isbn:3540653473","isbn:354044338X"]
   raw ".\n\n"  
 

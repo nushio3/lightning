a91938a0810100becd2c3fab8980fd5da8ebf513
Takayuki Muranushi <muranushi@google.com>
2014-02-25 22:02:21 +0900

    upload the latest progress.

--- paper7/src/Paper/SectionObservation.hs
+++ paper7/src/Paper/SectionObservation.hs
@@ -146,6 +146,8 @@ subsectionFigures = do
    \caption{
    Simulated Integrated Emission Map of  $\mathrm{HCO}^{+}$ lines.
    No lightning model (upper row) and Runaway breakdown model (lower row).
+   Units are in $\mathrm{Jy} ~ \mathrm{beam}^{ -1} ~ \mathrm{km} ~\mathrm{s}^{ -1}$
+   where beam area of 0.9 $\mathrm{arcsec}^{2}$ is assumed.
    }
    \end{figure}
    TODO: ALMA sensitivity estimations.

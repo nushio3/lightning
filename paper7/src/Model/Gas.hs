4f8ce7627ebf5c29545d207f48af6af47b2d98cf
Takayuki Muranushi <muranushi@google.com>
2014-04-25 17:04:05 +0900

    remove all the undefined symbols from Gas.hs

--- paper7/src/Model/Gas.hs
+++ paper7/src/Model/Gas.hs
@@ -2,7 +2,9 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
 
 module Model.Gas where
 
@@ -11,9 +13,16 @@ import Control.Lens as Lens
 import Text.Authoring
 import Text.Authoring.TH
 import Data.Metrology
+import Data.Metrology.AltOperators
 import Data.Metrology.Synonyms
+import Data.Metrology.SI.GenTypes
+import Data.Metrology.SI.Prefixes
+import Data.Metrology.SI.Units
+
+
 
 import Model.Values
+
 
 data ChemicalSpecies
   = H2
@@ -28,38 +37,39 @@ data ChemicalSpecies
   | N2HPlus
   deriving (Eq, Show)    
 
-airMix :: Convertible' a b => (ChemicalSpecies -> Value a b Double) -> Value a b Double
+airMix :: (ChemicalSpecies -> Qu d l Double) -> Qu d l Double
 airMix func = 0.78 *. func N2 |+| 0.21 *. func O2 |+| 0.01 *. func Ar
 
-ppdMix :: Convertible' a b => (ChemicalSpecies -> Value a b Double) -> Value a b Double
+ppdMix :: (ChemicalSpecies -> Qu d l Double) -> Qu d l Double
 ppdMix func = 0.9219 *. func H2 |+| 7.7718e-2 *. func He 
               |+| 2.262326e-4 *. func CO |+| 1.3404e-4 *. func O2
 
-atomicNumber :: ChemicalSpecies -> NoDimension Double 
-atomicNumber H2 = mkVal 2
-atomicNumber He = mkVal 2
-atomicNumber N2 = mkVal 14
-atomicNumber O2 = mkVal 16
-atomicNumber CO = mkVal 14
-atomicNumber Ar = mkVal 18
-atomicNumber H2O = mkVal 10
-atomicNumber HCOPlus = mkVal 15
-atomicNumber DCOPlus = mkVal 15
-atomicNumber N2HPlus = mkVal 15
+atomicNumber :: ChemicalSpecies -> Qu '[] l Double 
+atomicNumber H2 = scalar 2
+atomicNumber He = scalar 2
+atomicNumber N2 = scalar 14
+atomicNumber O2 = scalar 16
+atomicNumber CO = scalar 14
+atomicNumber Ar = scalar 18
+atomicNumber H2O = scalar 10
+atomicNumber HCOPlus = scalar 15
+atomicNumber DCOPlus = scalar 15
+atomicNumber N2HPlus = scalar 15
 
 
 
-molecularMass :: ChemicalSpecies -> GramPerMole Double 
-molecularMass H2 = mkVal 2
-molecularMass He = mkVal 4
-molecularMass N2 = mkVal 28
-molecularMass O2 = mkVal 32
-molecularMass CO = mkVal 28
-molecularMass Ar = mkVal 39.9
-molecularMass H2O = mkVal 18
-molecularMass HCOPlus = mkVal 29
-molecularMass DCOPlus = mkVal 30
-molecularMass N2HPlus = mkVal 29
+molecularMass :: ChemicalSpecies -> QuOfUL GramPerMole MySU
+molecularMass H2      =   2   % (Gram :/ Mole)
+molecularMass He      =   4   % (Gram :/ Mole)
+molecularMass N2      =  28   % (Gram :/ Mole)  
+molecularMass O2      =  32   % (Gram :/ Mole)
+molecularMass CO      =  28   % (Gram :/ Mole)
+molecularMass Ar      =  39.9 % (Gram :/ Mole)
+molecularMass H2O     =  18   % (Gram :/ Mole)
+molecularMass HCOPlus =  29   % (Gram :/ Mole)
+molecularMass DCOPlus =  30   % (Gram :/ Mole)
+molecularMass N2HPlus =  29   % (Gram :/ Mole)
+
 
 {- 
 Leiden大学のdataabaseがおすすめです。
@@ -71,66 +81,68 @@ http://www.sron.rug.nl/~vdtak/ratran/frames.html
 http://www.astro.uni-koeln.de/cdms
 -}
 
-rotationalConstant :: ChemicalSpecies -> PerSecond Double
-rotationalConstant HCOPlus = autoc $ speedOfLight |*| (mkVal 1.4875 :: PerCm Double)
-rotationalConstant DCOPlus = autoc $ speedOfLight |*| (mkVal 1.2015 :: PerCm Double)
-rotationalConstant N2HPlus = autoc $ speedOfLight |*| (mkVal 1.55395 :: PerCm Double)
+rotationalConstant :: ChemicalSpecies -> Frequency MySU Double
+rotationalConstant HCOPlus = speedOfLight |*| 1.4875 % (Number :/ Centi :@ Meter)
+rotationalConstant DCOPlus = speedOfLight |*| 1.2015  % (Number :/ Centi :@ Meter)
+rotationalConstant N2HPlus = speedOfLight |*| 1.55395  % (Number :/ Centi :@ Meter)
 
 rotationalConstant c       = error $ "rotational constant undefined for : " ++ show c
 
 
-ionizationEnergy :: ChemicalSpecies -> ElectronVolt Double
+ionizationEnergy :: ChemicalSpecies -> Energy MySU Double
 ionizationEnergy = go
   where
-    go H2  = mkVal 15.43
-    go N2  = mkVal 15.58
-    go CO  = mkVal 14.01
-    go O2  = mkVal 12.07
-    go H2O = mkVal 12.61
-    go Ar  = mkVal 15.76
-    go He  = mkVal 24.58
+    go H2  = 15.43 % ElectronVolt
+    go N2  = 15.58 % ElectronVolt
+    go CO  = 14.01 % ElectronVolt
+    go O2  = 12.07 % ElectronVolt
+    go H2O = 12.61 % ElectronVolt
+    go Ar  = 15.76 % ElectronVolt
+    go He  = 24.58 % ElectronVolt
     go _   = undefined
 
-inelCrossSection :: Int -> ChemicalSpecies -> Cm2 Double    
-inelCrossSection 12 N2 = mkVal 0.8e-16
-inelCrossSection 12 O2 = mkVal 1.8e-16
-inelCrossSection 12 Ar = mkVal 0
-inelCrossSection 15 H2 = mkVal 1.57e-16
-inelCrossSection 15 O2 = mkVal 1.81e-16
-inelCrossSection 15 CO = mkVal 0.051e-16
-inelCrossSection _  _  = mkVal 0
+inelCrossSection :: Int -> ChemicalSpecies -> Length MySU Double    
+inelCrossSection 12 N2 = 0.8e-16   % (Centi :@ Meter)
+inelCrossSection 12 O2 = 1.8e-16   % (Centi :@ Meter)
+inelCrossSection 12 Ar = 0         % (Centi :@ Meter)
+inelCrossSection 15 H2 = 1.57e-16  % (Centi :@ Meter)
+inelCrossSection 15 O2 = 1.81e-16  % (Centi :@ Meter)
+inelCrossSection 15 CO = 0.051e-16 % (Centi :@ Meter)
+inelCrossSection _  _  = 0         % (Centi :@ Meter)
 
-elCrossSection :: Int -> ChemicalSpecies -> Cm2 Double    
-elCrossSection 12 N2 = mkVal 11.6e-16
-elCrossSection 12 O2 = mkVal 9.00e-16
-elCrossSection 12 Ar = mkVal 17.4e-16
-elCrossSection 15 H2 = mkVal 6.62e-16
-elCrossSection 15 O2 = mkVal 8.89e-16
-elCrossSection 15 CO = mkVal 10.89e-16
-elCrossSection 15 He = mkVal 3.55e-16
-elCrossSection _  _  = mkVal 0
+elCrossSection :: Int -> ChemicalSpecies -> Length MySU Double    
+elCrossSection 12 N2 = 11.6e-16  % (Centi :@ Meter)
+elCrossSection 12 O2 = 9.00e-16  % (Centi :@ Meter)
+elCrossSection 12 Ar = 17.4e-16  % (Centi :@ Meter)
+elCrossSection 15 H2 = 6.62e-16  % (Centi :@ Meter)
+elCrossSection 15 O2 = 8.89e-16  % (Centi :@ Meter)
+elCrossSection 15 CO = 10.89e-16 % (Centi :@ Meter)
+elCrossSection 15 He = 3.55e-16  % (Centi :@ Meter)
+elCrossSection _  _  = 0         % (Centi :@ Meter)
 
-airDensity :: GramPerCm3 Double
-airDensity = mkVal 1.2041e-3
-
-airNumberDensity :: PerCm3 Double
-airNumberDensity = autoc $ airDensity |/| airMix molecularMass
+airDensity :: Density MySU Double
+airDensity = 1.2041 % (kilo Gram :/ Meter :^ pThree)
 
 
-mfpAir12 :: Cm Double
-mfpAir12 = autoc $ 1 /| airNumberDensity |/| (airMix $ inelCrossSection 12)
 
-mfpAir12E :: Cm Double
-mfpAir12E = autoc $ 1 /| airNumberDensity |/| (airMix $ elCrossSection 12)
+airNumberDensity :: QuOfUL PerCm3 MySU
+airNumberDensity = redim $ airDensity |/| airMix molecularMass |*| avogadroConstant
 
 
-airDielectricStrengthT :: VoltPerCm Double
-airDielectricStrengthT = autoc $ w |/| (mfpAir12 |*| elementaryCharge)
-  where w = mkVal 12 :: ElectronVolt Double
-                        
+mfpAir12 :: Length MySU Double
+mfpAir12 = redim $ 1 /| airNumberDensity |/| (airMix $ inelCrossSection 12)
+
+mfpAir12E :: Length MySU Double
+mfpAir12E = 1 /| airNumberDensity |/| (airMix $ elCrossSection 12)
 
 
-airDielectricStrengthDP :: VoltPerCm Double
+airDielectricStrengthT :: QuOfUL VoltPerCm MySU
+airDielectricStrengthT = redim $ w |/| (mfpAir12 |*| elementaryCharge)
+  where w = 12 % ElectronVolt
+                    
+
+{-
+airDielectricStrengthDP :: QuOfUL VoltPerCm MySU 
 airDielectricStrengthDP = autoc $ ratio *. w |/| (0.43 *. elementaryCharge |*| mfpAir12E) 
   where
     w = mkVal 12                           :: ElectronVolt Double
@@ -154,3 +166,4 @@ airDielectricStrengthR = autoc $
 
 
 
+-}
--- paper7/src/Model/Values.hs
+++ paper7/src/Model/Values.hs
@@ -1,58 +1,63 @@
-{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE ConstraintKinds, TypeOperators #-}
 module Model.Values where
 
 import Data.Metrology
 import Data.Metrology.Synonyms
 import Data.Metrology.SI.Units
+import qualified Data.Metrology.SI.Dims as D
 import Data.Metrology.SI.GenTypes
+import Data.Metrology.Z
 
 ----------------------------------------------------------------
 -- Mass units
 ----------------------------------------------------------------
 
-solarMass :: Compatible l Gram => Mass l Double
+solarMass :: Mass MySU Double
 solarMass = 1.98892e33 % Gram
 
-earthMass :: Compatible l Gram => Mass l Double
+earthMass :: Mass MySU Double
 earthMass = 5.9742e24 % Gram
 
-electronMass :: Compatible l Gram => Mass l Double
+electronMass :: Mass MySU Double
 electronMass = 9.10938291e-28 % Gram
 
-protonMass :: Compatible l Gram => Mass l Double
+protonMass :: Mass MySU Double
 protonMass = 1.67262178e-24 % Gram
 
 
-speedOfLight :: Compatible l CmPerSec => Velocity l Double
-speedOfLight =  29979245800 % (undefined :: CmPerSec)
+speedOfLight :: Velocity MySU Double
+speedOfLight =  299792458 % (Meter :/ Second)
 
 
-elementaryCharge :: (Compatible l Coulomb) => Charge l Double
+elementaryCharge :: Charge MySU Double
 elementaryCharge = 1.60217657e-19 % Coulomb
 
 
-gravitationalConstant :: Compatible l SIGCUnit => QuOfUL SIGCUnit l
+gravitationalConstant :: QuOfUL SIGCUnit MySU
 gravitationalConstant = 6.67384e-11 % (undefined :: SIGCUnit)
 
-kB :: Compatible l SIkBUnit => QuOfUL SIkBUnit l
+kB :: QuOfUL SIkBUnit MySU
 kB = 1.3806488e-23 % (undefined :: SIkBUnit)
 
 
+planckLength :: Length MySU Double
+planckLength = qSqrt (hbar |*| gravitationalConstant |/| (speedOfLight |^ pThree))
+
 -- eps0
-vacuumPermittivity :: Compatible l SIPermittivityUnit
-  => QuOfUL SIPermittivityUnit l
+vacuumPermittivity :: QuOfUL SIPermittivityUnit MySU
 vacuumPermittivity =  
   (1 / (4 * pi * 1e-7 * 299792458**2)) % (undefined :: SIPermittivityUnit)
 -- mu0                     
-vacuumPermeability :: Compatible l SIPermeabilityUnit 
-  => QuOfUL SIPermeabilityUnit l
+vacuumPermeability :: QuOfUL SIPermeabilityUnit MySU
 vacuumPermeability = (4 * pi * 1e-7) % (undefined :: SIPermeabilityUnit)
 
 -- |Planck constant
-planckConstant :: Compatible l JouleSecond 
-  => QuOfUL JouleSecond l
+planckConstant :: QuOfUL JouleSecond MySU
 planckConstant =  (6.6260695729e-34) % (undefined :: JouleSecond)
 
 -- |Reduced Planck constant
-hbar ::  Compatible l JouleSecond   => QuOfUL JouleSecond l
-hbar = (1 / 2 / pi) *. planckConstant
+hbar :: QuOfUL JouleSecond MySU
+hbar = (1 / 2 / pi) *| planckConstant
+
+avogadroConstant :: MkGenQu (D.AmountOfSubstance :^ MOne) MySU Double
+avogadroConstant = 6.02214129e23 % (Number :/ Mole)

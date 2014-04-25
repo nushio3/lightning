3329f232168b3a9cbb3677e281816d2485066460
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-04-25 16:24:48 +0900

    locate a source critical error (but postpone its fix)

--- paper7/src/Model/Dust.hs
+++ paper7/src/Model/Dust.hs
@@ -9,8 +9,9 @@ import           Data.Monoid ((<>))
 import qualified Text.LaTeX as LTX
 import qualified Text.LaTeX.Base.Class as LTX
 import qualified Text.LaTeX.Packages.AMSMath as LTX
+import           Data.Metrology
 import           Data.Metrology.Synonyms
 
 
-dustSurfaceCharge :: CoulombPerCm2 Double
-dustSurfaceCharge = mkVal 1e-10
+dustSurfaceCharge :: QuOfUL  CoulombPerCm2 MySU 
+dustSurfaceCharge = 1e-10 % (undefined :: CoulombPerCm2)

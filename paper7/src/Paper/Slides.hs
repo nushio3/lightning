f3d127ac7bdf71addaff87831399b7c00630c6e1
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-05-01 11:37:50 +0900

    produce a page of slides in units.

--- paper7/src/Paper/Slides.hs
+++ paper7/src/Paper/Slides.hs
@@ -23,7 +23,10 @@ import qualified Text.LaTeX as LTX
 
 import           Model.Gas
 
+import           Data.Metrology
 import           Data.Metrology.Synonyms
+import           Data.Metrology.SI.Units
+import           Data.Metrology.SI.Prefixes
 import           Data.Metrology.LaTeX
 
 
@@ -61,17 +64,46 @@ genBodyText = do
 
 sectionSlides :: MonadAuthoring s w m => m ()
 sectionSlides = do
+  let cm2 = centi Meter :^ pTwo
+      cm3 = centi Meter :^ pThree
+      cmM3 = centi Meter :^ pMThree      
+      kvcm = kilo Volt :/ centi Meter
+  
   command1 "section" $ raw "Method"
   environment "frame" $ do
     environment "tabular" $ do
+          
       raw "{c|ccc}"
+      [rawQ| \hline \hline |]
       [rawQ|  & $\rm N_2$ & $\rm O_2$ & $\rm Ar$ \\ |]
+      [rawQ| \hline |]
       [rawQ| Volume fraction & 78\% & 21\% & 1\% \\|]
       [rawQ| $\Delta W$ 
-             & #{ppF "%.2f" ElectronVolt $ ionizationEnergy N2}   
-             & #{ppF "%.2f" ElectronVolt $ ionizationEnergy O2}   
-             & #{ppF "%.2f" ElectronVolt $ ionizationEnergy Ar}   
+             & $#{ppF ElectronVolt "%.2f" $ ionizationEnergy N2}$   
+             & $#{ppF ElectronVolt "%.2f" $ ionizationEnergy O2}$   
+             & $#{ppF ElectronVolt "%.2f" $ ionizationEnergy Ar}$   
              \\|]      
+      [rawQ| $\sigma_{\rm inel}$ 
+             & $#{ppFP cm2 "%.1f" (-16) $ inelCrossSection 12 N2}$   
+             & $#{ppFP cm2 "%.1f" (-16) $ inelCrossSection 12 O2}$   
+             & $#{ppFP cm2 "%.1f" (-16) $ inelCrossSection 12 Ar}$   
+             \\|]     
+      [rawQ| \hline |]
+    environment "itemize" $ do
+      [rawQ| 
+        \item
+          Air number density at NTP is $#{ppE cmM3 3 airNumberDensity}$ 
+        \item
+          The mean inelastic cross section of air at 12eV is 
+          $#{ppE cm2 1 $ airMix $ inelCrossSection 12}$.
+        \item
+          $l_{\rm mfp} = (n_n \sigma_{inel})^{ -1 } = #{ppE (centi Meter) 1 mfpAir12}$ 
+        \item
+         This gives 
+         $E_{\rm crit} = #{ppF kvcm "%.2f" $ airDielectricStrengthT}$,
+         which is in good agreement with lab experiments. |]
+
+
   environment "frame" $ do
     environment "itemize" $ do
       raw "\\item meter \\pause"            

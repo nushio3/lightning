5a69191711d85ab160b636923e33f556414ef21c
Takayuki Muranushi <muranushi@google.com>
2014-07-14 16:19:26 +0900

    add figures far, far away

--- paper7/attic/calc-norm.py
+++ paper7/attic/calc-norm.py
@@ -15,9 +15,9 @@ dir = 'material-old/lime-output/'
 
 for chem in chems:
   for [a1,b1] in models:
-    fn1 = dir + 'LgRg16k-%s-%s-R50_100-V200.0x201.fits' % (chem , a1)
+    fn1 = dir + 'LgRg16k-%s-%s-R100_200-V200x201.fits' % (chem , a1)
     for [a2,b2] in models:
-      fn2 = dir + 'LgRg16k-%s-%s-R50_100-V200.0x201.fits' % (chem , a2)
+      fn2 = dir + 'LgRg16k-%s-%s-R100_200-V200x201.fits' % (chem , a2)
       h1 =  pyfits.open(fn1)[0].data
       h2 =  pyfits.open(fn2)[0].data
       norm = ((h1-h2)**2).sum()

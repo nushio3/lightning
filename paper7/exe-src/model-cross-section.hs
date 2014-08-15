7301e2447ecd3b6a6793e3a31aea3233dc78715a
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-08-15 23:33:21 +0900

    the fitting result has been provided.

--- paper7/exe-src/model-cross-section.hs
+++ paper7/exe-src/model-cross-section.hs
@@ -1,4 +1,11 @@
-import Control.Monad
+{-# LANGUAGE RecordWildCards, DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+
+import Control.Monad hiding (mapM, mapM_, forM_)
+import Numeric.Optimization.Algorithms.CMAES
+
+import Data.Foldable
+import Data.Traversable 
+import qualified Data.Map as M
 
 import Data.Metrology.Poly
 import Data.Metrology.Synonyms
@@ -6,13 +13,44 @@ import Data.Metrology.SI.Prefixes
 import Data.Metrology.SI.Units
 import Data.Metrology.Z
 
+import Prelude hiding (sum,mapM, mapM_)
+
 import Model.Gas
 
 data ExperimentData
   = ExperimentData
   { dataFilename :: FilePath
   , ion      :: ChemicalSpecies
-  , target   :: ChemicalSpecies}
+  , target   :: ChemicalSpecies
+  , i2cs     :: M.Map Int Double }
+
+data ModelTC a = Model
+  { paramA :: a
+  , paramP :: a
+  , paramQ :: a
+  } deriving (Eq, Show, Read, Functor, Foldable, Traversable)
+
+type Model = ModelTC Double
+
+defaultModel = Model 100 0 0
+
+predict :: Model -> ExperimentData -> Double
+predict Model{..} xp = paramA * mu**paramP * r**paramQ
+  where
+    (mu,r) = massesD xp
+  
+errorAt :: Model -> ExperimentData -> Int -> Double
+errorAt m xp i = (log valM - log valX) ** 2
+  where
+    valM = max 1e-99 $ predict m xp
+    Just valX = M.lookup i (i2cs xp)
+
+fitModel :: [ExperimentData] -> Int -> IO Model
+fitModel xps i = do
+  run $ minimizeT f defaultModel
+  where
+    f :: Model -> Double
+    f m = sum [errorAt m xp i | xp <- xps]
 
 massesD :: ExperimentData -> (Double, Double)
 massesD xp = (mu, r) 
@@ -32,7 +70,7 @@ inputData =
     e "H3+_H2.txt" H3Plus H2 ,
     e "N+_N2.txt"  NPlus  N2 ,
     e "N2+_N2.txt" N2Plus N2 ]
-   where e = ExperimentData
+   where e f a b = ExperimentData f a b M.empty
 
 
 
@@ -40,10 +78,14 @@ inputData =
 
 main :: IO ()
 main = do
-  mapM_ bucket inputData
+  loadedData <- mapM loadFile inputData
+  forM_ [-8..32] $ \i -> do
+    m <- fitModel loadedData i
+    print m
+  return ()
   
-bucket :: ExperimentData -> IO ()
-bucket xp = do
+loadFile :: ExperimentData -> IO ExperimentData
+loadFile xp = do
   let fn = dataFilename xp
   con <- readFile $ "material/cross-section/" ++ fn
   let xys :: [(Int, Double)]
@@ -54,3 +96,4 @@ bucket xp = do
       bin x = round $ log x / log 10 * 8 
   mapM_ print xys
   print $ massesD xp
+  return xp{i2cs = M.fromList xys}

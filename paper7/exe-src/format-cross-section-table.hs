ac1b36571b4b8a195ec0cf8a9d2f4b0f7a0d6a29
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-08-15 12:50:22 +0900

    fix OCR errors in cross section table.

--- paper7/exe-src/format-cross-section-table.hs
+++ paper7/exe-src/format-cross-section-table.hs
@@ -1,3 +1,16 @@
 main :: IO ()
 main = do
-  return ()
+  con <- getContents
+  parse con
+  
+parse :: String -> IO ()
+parse con = do
+  mapM_ putStrLn $ filter isHeadline lc
+  where
+    lc :: [String]
+    lc = lines con
+    
+    isHeadline :: String -> Bool
+    isHeadline = 
+      not . null .
+      filter (not . flip elem "0123456789. \t")

c4c4f7fbb6e42e3fe7b678b0a6ce5f3943795363
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-08-15 18:01:02 +0900

    it is shown that you can bin the cross sections.

---
+++ paper7/exe-src/model-cross-section.hs
@@ -0,0 +1,25 @@
+import Control.Monad
+
+files :: [FilePath]
+files = ["Ar+_Ar.txt",
+         "H+_H2.txt",
+         "H2+_H2.txt",
+         "H3+_H2.txt",
+         "N+_N2.txt",
+         "N2+_N2.txt"]
+
+
+main :: IO ()
+main = do
+  mapM_ bucket files
+  
+bucket :: FilePath -> IO ()
+bucket fn = do
+  con <- readFile $ "material/cross-section/" ++ fn
+  let xys :: [(Int, Double)]
+      xys = [(bin $ read sx, read sy) |
+             [sx,sy] <- map words $ lines con]
+      
+      bin :: Double -> Int
+      bin x = round $ log x / log 10 * 8 
+  mapM_ print xys

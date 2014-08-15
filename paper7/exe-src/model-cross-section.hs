81268b9fad484cb79138722119e1ebd239f4ee04
nushio <nushio@nuwheezy.paraiso-lang.org>
2014-08-15 22:14:22 +0900

    test printout ion/target masses for cross section experiments

--- paper7/exe-src/model-cross-section.hs
+++ paper7/exe-src/model-cross-section.hs
@@ -1,25 +1,46 @@
 import Control.Monad
 
-data Dataset
-  = Dataset
-  { csFilename :: FilePath }
+import Data.Metrology.Poly
+import Data.Metrology.Synonyms
+import Data.Metrology.SI.Prefixes
+import Data.Metrology.SI.Units
+import Data.Metrology.Z
 
-files :: [FilePath]
-files = ["Ar+_Ar.txt",
-         "H+_H2.txt",
-         "H2+_H2.txt",
-         "H3+_H2.txt",
-         "N+_N2.txt",
-         "N2+_N2.txt"]
+import Model.Gas
+
+data ExperimentData
+  = ExperimentData
+  { dataFilename :: FilePath
+  , ion      :: ChemicalSpecies
+  , target   :: ChemicalSpecies}
+
+massesD :: ExperimentData -> (Double, Double)
+massesD xp = (m1, m2) 
+  where
+    m1 = molecularMass (ion xp) # (Gram :/ Mole)
+    m2 = molecularMass (target xp) # (Gram :/ Mole)
+  
+inputData :: [ExperimentData]
+inputData =
+   [e "Ar+_Ar.txt" ArPlus Ar ,
+    e "H+_H2.txt"  HPlus  H2 ,
+    e "H2+_H2.txt" H2Plus H2 ,
+    e "H3+_H2.txt" H3Plus H2 ,
+    e "N+_N2.txt"  NPlus  N2 ,
+    e "N2+_N2.txt" N2Plus N2 ]
+   where e = ExperimentData
+
+
 
 
 
 main :: IO ()
 main = do
-  mapM_ bucket files
+  mapM_ bucket inputData
   
-bucket :: FilePath -> IO ()
-bucket fn = do
+bucket :: ExperimentData -> IO ()
+bucket xp = do
+  let fn = dataFilename xp
   con <- readFile $ "material/cross-section/" ++ fn
   let xys :: [(Int, Double)]
       xys = [(bin $ read sx, read sy) |
@@ -28,4 +49,4 @@ bucket fn = do
       bin :: Double -> Int
       bin x = round $ log x / log 10 * 8 
   mapM_ print xys
-  
+  print $ massesD xp

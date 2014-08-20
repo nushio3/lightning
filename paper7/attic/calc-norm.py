b4417bc5e78f62e591a4aa1d87c460e347c09a07
Takayuki Muranushi <muranushi@google.com>
2014-08-20 14:31:09 +0900

    update the norm.

--- paper7/attic/calc-norm.py
+++ paper7/attic/calc-norm.py
@@ -11,15 +11,35 @@ models = [['N', 'Nothing'],
 chems = ['HCOPlus', 'DCOPlus', 'N2HPlus']
 
 
-dir = 'material-old/lime-output/'
+dir = 'material/lime-output/'
+
+fp = open('src/Model/modelNorm25.hs','w')
 
 for chem in chems:
   for [a1,b1] in models:
-    fn1 = dir + 'LgRg16k-%s-%s-R100_200-V200x201.fits' % (chem , a1)
+    fn1 = dir + 'LgAK16k-%s-%s-R25_50-V200x201.fits' % (chem , a1)
     for [a2,b2] in models:
-      fn2 = dir + 'LgRg16k-%s-%s-R100_200-V200x201.fits' % (chem , a2)
+      fn2 = dir + 'LgAK16k-%s-%s-R25_50-V200x201.fits' % (chem , a2)
       h1 =  pyfits.open(fn1)[0].data
       h2 =  pyfits.open(fn2)[0].data
       norm = ((h1-h2)**2).sum()
-      msg = 'modelNorm %s %s %s = %e' % (chem, b1 , b2 , norm)
-      print msg
+      msg = "modelNorm %s %s %s = %e\n" % (chem, b1 , b2 , norm)
+      fp.write(msg)
+
+fp.close()
+
+
+fp = open('src/Model/modelNorm50.hs','w')
+
+for chem in chems:
+  for [a1,b1] in models:
+    fn1 = dir + 'LgAK16k-%s-%s-R50_100-V200x201.fits' % (chem , a1)
+    for [a2,b2] in models:
+      fn2 = dir + 'LgAK16k-%s-%s-R50_100-V200x201.fits' % (chem , a2)
+      h1 =  pyfits.open(fn1)[0].data
+      h2 =  pyfits.open(fn2)[0].data
+      norm = ((h1-h2)**2).sum()
+      msg = "modelNorm %s %s %s = %e\n" % (chem, b1 , b2 , norm)
+      fp.write(msg)
+
+fp.close()
